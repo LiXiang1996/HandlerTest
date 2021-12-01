b4d2b48d86b99affae9f79dd44736f2a9405605e
mac <lixiang@lerjin.onaliyun.com>
2021-12-01 17:41:53 +0800

    1

---
+++ app/src/main/java/com/example/handlertest/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.handlertest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
